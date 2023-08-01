9e2912425255b0113cab83ef14a391b0777ece5e
gautamyadav <gautamyadav@spliceglobal.com>
2023-08-01 18:43:42 +0530

    3nd

--- gautamyadav/urls.py
+++ gautamyadav/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from system import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.homepage),
     path('<int:timeframe>', views.goals_by_int_timeframe),
     path('<str:timeframe>', views.goals_by_timeframe, name='namedurl'),
 ]
+# git init
+# git add .
+# git commit -m "Initial commit"
+# git push -u origin master
