661a8f8573e98f6581030dc079c893f287584449
gautamyadav <gautamyadav@spliceglobal.com>
2023-08-02 11:05:58 +0530

    'aaaa'

--- system/views.py
+++ system/views.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 from django.http import HttpResponse
 
 def homepage(request):
     return HttpResponse("hello Gautam Yadav")
-=======
-from django.http import HttpResponse
-
-def homepage(request):
-    return HttpResponse("hello Gautam Yadav")
-    # return HttpResponse("hello Gautam Yadav")
->>>>>>> cd74c3327a5d238fc22edd794b97da59d2e351ce
