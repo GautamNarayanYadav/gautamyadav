65d84991df98e36e1ddb873825c8e50818cae857
GautamNarayanYadav <imgautamyadav@gmail.com>
2024-02-13 23:21:25 +0530

    hi

--- system/views.py
+++ system/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import *
 
 from django.http import HttpResponse
@@ -8,7 +11,7 @@ def homepage(request):
     template_name = 'index.html'
     return render(request, template_name)
 
-
+@permission_classes([IsAuthenticated])
 def track_visit(request):
     ip_address = request.META.get('REMOTE_ADDR')
     user_agent = request.META.get('HTTP_USER_AGENT')
