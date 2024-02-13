a1771fb4fba42b9b4bb3528238a1a5c19f54b8a8
GautamNarayanYadav <imgautamyadav@gmail.com>
2024-02-13 23:15:44 +0530

    hi

--- gautamyadav/urls.py
+++ gautamyadav/urls.py
@@ -3,13 +3,15 @@ from django.contrib import admin
 
 from system import views
 from accounts.views import *
+from system.views import track_visit
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', views.homepage, name='profile'),
     path('', profileListView.as_view(), name="profile"),
     path('api/accounts/', include('accounts.urls')),
-    path('api/system/', include('system.urls'))
+    path('api/system/', include('system.urls')),
+    path('track-visit/', track_visit, name='track-visit'),
 ]
 
 # git init
