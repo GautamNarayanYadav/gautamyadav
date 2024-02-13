51305391b6d03490b6575872e881028a44011e27
GautamNarayanYadav <imgautamyadav@gmail.com>
2024-02-13 22:37:14 +0530

    Initial commit

--- system/admin.py
+++ system/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from system.models import *
 
+
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = ('id', 'name', 'email', 'subject', 'message', 'datetime')
+    readonly_fields = ('datetime', )
+
+
+admin.site.register(Message, MessageAdmin)
 admin.site.register(MyProfile)
-admin.site.register(Message)
+
+
